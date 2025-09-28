08257b05f9d32d3c12bb3e159277e57b62160e78
hiurydev <hiury.ronchi@icloud.com>
2025-09-28 12:25:50 -0300

    breeze auth

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0', // escuta dentro do container
+        port: 5173,
+        hmr: {
+            host: 'localhost', // ESSENCIAL: browser acessa localhost
+            protocol: 'ws',    // WebSocket
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
 });
